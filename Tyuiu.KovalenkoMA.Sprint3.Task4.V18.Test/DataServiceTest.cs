65087f975fe42935854deccd0962af542a086e6d
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-10-27 23:08:28 +0500

    Task4

---
+++ Tyuiu.KovalenkoMA.Sprint3.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KovalenkoMA.Sprint3.Task4.V18.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint3.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            var res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(res, 2.993);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint3.Task4.V18/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KovalenkoMA.Sprint3.Task4.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Дано:" + startValue);
+        Console.WriteLine("Дано:" + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
